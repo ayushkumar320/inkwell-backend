07d152a461bd192da749d1fd0b1824eb551f23ac
Ayush <ayushkumar85385@gmail.com>
2025-10-24 10:06:04 +0530

    added login controller with jwt signature

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,6 +4,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
+const JWT_SECRET = process.env.JWT_SECRET as string;
 
 export async function registerUser(req: Request, res: Response) {
   const {email, firstName, lastName, password} = req.body;
@@ -52,5 +53,61 @@ export async function registerUser(req: Request, res: Response) {
         error: "An error occurred while registering the user",
       }
     })
+  }
+}
+
+export async function loginUnser(req: Request, res: Response) {
+  const {email, password} = req.body;
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    if (!user) {
+      return res.status(400).json({
+        status: {
+          code: 400,
+          message: "Invalid email or password",
+        }, data: {
+          error: "User not found",
+        }
+      });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({
+        status: {
+          code: 400,
+          message: "Invalid email or password",
+        }, data: {
+          error: "Incorrect email or password",
+        }
+      });
+    }
+    const token = jwt.sign({email: user.email}, JWT_SECRET);
+    return res.status(200).json({
+      status: {
+        code: 200,
+        message: "Login successful",
+      }, data: {
+        user: {
+          id: user.id,
+          email: user.email,
+          firstName: user.firstName,
+          lastName: user.lastName,
+        },
+        token: token,
+      }
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: {
+        code: 500,
+        message: "Internal Server Error",
+      }, data: {
+        error: "An error occurred while logging in",
+      }
+    });
   }
 }
