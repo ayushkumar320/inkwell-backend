0ccd49ddbeee8939c9d44176edd4cd2cb737f604
Ayush <ayushkumar85385@gmail.com>
2025-10-26 11:15:27 +0530

    formatted using prettier

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,11 +1,14 @@
 import express from 'express';
-import {registerUser, loginUser, updateUserProfile} from '../controllers/user.controller.js';
+import {
+  registerUser,
+  loginUser,
+  updateUserProfile,
+} from '../controllers/user.controller.js';
 import {userAuthMiddleware} from '../middleware/user.auth.js';
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.put('/profile', userAuthMiddleware, updateUserProfile);
-
 
 export default router;
