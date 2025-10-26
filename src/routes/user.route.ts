5be2516d4c6a83bc0f7b7ca6f88950955991367e
Ayush <ayushkumar85385@gmail.com>
2025-10-26 11:15:01 +0530

    added route for update profile

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import {registerUser, loginUser} from '../controllers/user.controller.js';
+import {registerUser, loginUser, updateUserProfile} from '../controllers/user.controller.js';
 import {userAuthMiddleware} from '../middleware/user.auth.js';
-
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.put('/profile', userAuthMiddleware, updateUserProfile);
+
 
 export default router;
